e17d6a1fc67c389e1856ab91ddb1f05c8254eab7
Jay Patel <81924044+jayp822@users.noreply.github.com>
2024-11-11 13:20:16 -0500

    try

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,12 +6,12 @@ import node from "@astrojs/node";
 
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: "server",
 
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: vercel(),
 });
