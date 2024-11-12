e321838bd36758e91e0a9f7caf770b097ea3a1b1
Jay Patel <81924044+jayp822@users.noreply.github.com>
2024-11-11 20:39:04 -0500

    finished about me

--- src/components/Email/Contact.jsx
+++ src/components/Email/Contact.jsx
@@ -105,10 +105,10 @@ export default function Contact() {
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                class="lucide lucide-check"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="lucide lucide-check"
               >
                 <path d="M20 6 9 17l-5-5" />
               </svg>
@@ -123,10 +123,10 @@ export default function Contact() {
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                class="lucide lucide-send z-40"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="lucide lucide-send z-40"
               >
                 <path d="M14.536 21.686a.5.5 0 0 0 .937-.024l6.5-19a.496.496 0 0 0-.635-.635l-19 6.5a.5.5 0 0 0-.024.937l7.93 3.18a2 2 0 0 1 1.112 1.11z" />
                 <path d="m21.854 2.147-10.94 10.939" />
