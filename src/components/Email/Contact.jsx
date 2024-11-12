e1faad5d52eaeab3f0625ca3e1b1f8446892af50
Jay Patel <81924044+jayp822@users.noreply.github.com>
2024-11-12 18:50:49 -0500

    ok

--- src/components/Email/Contact.jsx
+++ src/components/Email/Contact.jsx
@@ -41,9 +41,12 @@ export default function Contact() {
 
   return (
     <div className="mb-[100px] flex w-full flex-col">
-      <h1 className="relative mx-auto mb-[40px] mt-[8rem] inline-block text-nowrap text-center text-2xl font-bold before:absolute before:-bottom-1 before:left-0 before:h-[0.3rem] before:w-full before:rounded-full before:bg-pink-500 sm:text-3xl lg:text-4xl">
+      <h1 className="relative mx-auto mt-[8rem] inline-block text-nowrap text-center text-2xl font-bold before:absolute before:-bottom-1 before:left-0 before:h-[0.3rem] before:w-full before:rounded-full before:bg-pink-500 sm:text-3xl lg:text-4xl">
         Contact
       </h1>
+      <p class="mb-[40px] mt-[20px] text-center text-base text-neutral-300 sm:text-lg lg:text-xl">
+        Feel free to message me about new opportunities or projects
+      </p>
       <form
         ref={formRef}
         onSubmit={handleSubmit}
@@ -89,10 +92,10 @@ export default function Contact() {
         <button
           type="submit"
           disabled={isSubmited} // Disable button if isSubmited is true
-          className={`flex w-[65%] max-w-[13rem] items-center justify-center gap-2 overflow-hidden rounded-full text-center md:w-[55%] lg:w-[45%] ${
+          className={`flex w-[65%] max-w-[11rem] items-center justify-center gap-2 overflow-hidden rounded-full text-center md:w-[55%] lg:w-[45%] ${
             isSubmited
               ? "bg-blue-600 opacity-60"
-              : "relative bg-blue-600 shadow-2xl transition-all duration-200 ease-out before:absolute before:h-0 before:w-0 before:rounded-full before:bg-pink-500 before:duration-300 before:ease-out focus:ring-2 focus:ring-blue-500 sm:hover:shadow-pink-500 sm:hover:before:h-[11rem] sm:hover:before:w-[13rem]"
+              : "relative bg-blue-600 shadow-2xl transition-all duration-200 ease-out before:absolute before:h-0 before:w-0 before:rounded-full before:bg-pink-500 before:duration-300 before:ease-out focus:ring-2 focus:ring-blue-500 sm:hover:shadow-pink-500 sm:hover:before:h-[11rem] sm:hover:before:w-[11rem]"
           } px-4 py-2 text-white focus:outline-none`}
         >
           {isSubmited ? (
